6e90511c70a6abaf30e9a449cd492ce2646ce451
kyleotero <kkyleotero@gmail.com>
2022-08-18 18:46:34 -0400

    added scroll onclick to navbar

--- lscug-v2/src/components/navbar.jsx
+++ lscug-v2/src/components/navbar.jsx
@@ -1,13 +1,14 @@
 import React, {useRef} from 'react';
+import { Link } from 'react-scroll';
 
 export default function NavBar(){
     return  <div id='topNav' className='w-full h-1/12 relative'>
                 <div className='flex justify-center select-none text-xl font-biko text-black dark:text-white'>
-                    <a id="curriculum" className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black">Curriculum</a>
-                    <a id="miscellaneous" className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black">Miscellaneous</a>
-                    <a id="coop" className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black">Co-op & Internships</a>
-                    <a id="faq" className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black">FAQ</a>
-                    <a id="author" className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black">About Author</a>     
+                    <a className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black"><Link to="curriculum" smooth={true} duration={300} offset={-20}>Curriculum</Link></a>
+                    <a className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black"><Link to="misc" smooth={true} duration={300} offset={-20}>Miscellaneous</Link></a>
+                    <a className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black"><Link to="coop" smooth={true} duration={300} offset={-20}>Co-op & Internships</Link></a>
+                    <a className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black"><Link to="faq" smooth={true} duration={300} offset={-20}>FAQ</Link></a>
+                    <a className="transition ease-in-out duration-300 hover:cursor-pointer p-3 m-10 shadow-prelight dark:shadow-pre hover:shadow-boxlight dark:hover:shadow-box hover:text-white dark:hover:text-black"><Link to="about" smooth={true} duration={300} offset={-20}>About Author</Link></a>     
               </div>
               <div id="horizontalLine" className='relative flex justify-center'>
                 <div className='w-1/3 border-t border-black dark:border-white'></div>
