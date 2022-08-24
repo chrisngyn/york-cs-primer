551f97129c111b59587089783b3a00ff4c1d3077
kyleotero <kkyleotero@gmail.com>
2022-08-24 12:08:49 -0400

    smoothing animations

--- src/components/textdrop.jsx
+++ src/components/textdrop.jsx
@@ -6,7 +6,6 @@ export default function TextDrop(props) {
   const [effect, setEffect] = useState(false)
   const [height, setHeight] = useState(0)
   const [style, setStyle] = useState(null)
-  const [style2, setStyle2] = useState(null)
   const [toggle, setToggle] = useState(false)
 
   useEffect(() => {
@@ -17,11 +16,9 @@ export default function TextDrop(props) {
   const handleClick = () => {
     if (effect === false) {
       setStyle({ height: 96 + height })
-      setStyle2({ height: height })
       setTimeout(auto, 100)
     } else if (effect === true) {
       setStyle({ height: 96 + height })
-      setStyle2({ height: height })
       setTimeout(close, 1)
     }
     setEffect(current => !current)
@@ -29,12 +26,10 @@ export default function TextDrop(props) {
 
   const auto = event => {
     setStyle({ height: "auto" })
-    setStyle2({ height: "auto" })
   }
 
   const close = event => {
     setStyle({ height: 96 })
-    setStyle2({ height: 96 })
   }
 
   return (
@@ -44,7 +39,7 @@ export default function TextDrop(props) {
         <img className={`${effect && "animate-flip"} h-6 w-6 lg:h-8 lg:w-8 right-6 lg:right-8 dark:invert absolute`} src={Down} alt="down arrow" />
         <div className='w-[95%] lg:w-90% px-5 dark:text-white text-sm xl:text-xl break-words'>{props.name}</div>
       </div>
-      <div ref={ref} style={style2} className={`${effect ? "visible" : "invisible"} h-0 pl-5 pr-5 pb-3 overflow-hidden transition-all text-sm xl:text-lg whitespace-pre-line dark:text-white`}>
+      <div ref={ref} className={`${effect ? "visible" : "invisible"} pl-5 pr-5 pb-3 overflow-hidden transition-all text-sm xl:text-lg whitespace-pre-line dark:text-white`}>
         {toggle && (<div className={`${effect ? "visible" : "invisible"} $ text-sm lg:text-xl whitespace-pre-line dark:text-white`}>{props.desc}</div>)}
         {props.text}
       </div>
